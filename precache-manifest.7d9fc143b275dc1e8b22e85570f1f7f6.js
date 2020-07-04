80d4b20ad1016a438bc35071596ba14e8ab79333
Youssef <eryous.ye@yahoo.com>
2020-07-04 01:06:28 -0400

    Updates

---
+++ precache-manifest.7d9fc143b275dc1e8b22e85570f1f7f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc194e41bad84c154ff21e754e367dfc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3097e6efb7ee80e4b2cb",
+    "url": "/my-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1b1979d692375653f629",
+    "url": "/my-app/static/css/main.6f32bd38.chunk.css"
+  },
+  {
+    "revision": "3097e6efb7ee80e4b2cb",
+    "url": "/my-app/static/js/2.15202ba0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-app/static/js/2.15202ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1979d692375653f629",
+    "url": "/my-app/static/js/main.074d7794.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "d38383951e427467ff8645a3c825e72c",
+    "url": "/my-app/static/media/profilePic.d3838395.jpg"
+  }
+]);
