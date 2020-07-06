bac3d69c0e041d3adef2bf0e5220705909c3ec54
Youssef <eryous.ye@yahoo.com>
2020-07-06 05:36:48 -0400

    Updates

---
+++ precache-manifest.9f84595d766a4bf9492938ac2f4fdecc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3bd18449bae1c53bd1b2d58c334e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cba43265f276bb123fa",
+    "url": "/static/css/2.db6f3515.chunk.css"
+  },
+  {
+    "revision": "0082b495a9e70ca6310d",
+    "url": "/static/css/main.89bec2a6.chunk.css"
+  },
+  {
+    "revision": "8cba43265f276bb123fa",
+    "url": "/static/js/2.ca0e9dfc.chunk.js"
+  },
+  {
+    "revision": "6485e28f1bc40c1e743bb4de5f286310",
+    "url": "/static/js/2.ca0e9dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0082b495a9e70ca6310d",
+    "url": "/static/js/main.4f5be2d4.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "81f3da7e37d5fdc1ccd9c7f20863301d",
+    "url": "/static/media/jumbotron_bg.81f3da7e.jpg"
+  },
+  {
+    "revision": "d38383951e427467ff8645a3c825e72c",
+    "url": "/static/media/profilePic.d3838395.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.c10c9759b0d97a8a40ddde925375ba3e.js"
+  "/precache-manifest.9f84595d766a4bf9492938ac2f4fdecc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
